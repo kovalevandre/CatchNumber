f536dc4a8e9f11876786e351603d7b04f4801066
Andrey <akovalev@me.com>
2023-12-21 23:12:55 +0300

    Initial Commit

---
+++ CatchNumber/CatchNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchNumberApp.swift
+//  CatchNumber
+//
+//  Created by Andrey Kovalev on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CatchNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
